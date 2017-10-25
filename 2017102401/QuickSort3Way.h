0d28c1d8966887824e9b8c78a94ade434bdb8fc5
githubgjb <86742187@qq.com>
2017-10-25 14:31:47 +0800

    QuickSort3WayOver

--- 2017102401/QuickSort3Way.h
+++ 2017102401/QuickSort3Way.h
@@ -47,9 +47,43 @@ void quickSort(T arr[],int n){
 }
 
 
-
+//三路快速排序 arr[l...r]
+//将arr[l...r]分为<v; ==v ; >v 三部分
+//之后递归对 <V ; >V 两部分继续进行三路快速排序 
 template <typename T>
-void //todo 未完...待续...
+void __quickSort3Ways(T arr[],int l,int r){
+	
+	if(r-l <= 15){
+		insertionSort(arr,l,r);
+		return;
+	}
+	
+	//partition
+	swap( arr[l],arr[rand()%(r-l+1)+l]);
+	T v = arr[l];
+	
+	int lt = l; //arr[l+1...lt] < v
+	int gt = r + 1; //arr[gt...r] >v 
+	int i = l + 1; //arr[lt+1...i) == v;
+	while(i < gt){
+		if(arr[i] < v){
+			swap(arr[i] , arr[lt+1]);
+			lt++;
+			i++;
+			
+		}else if(arr[i] > v){
+			swap(arr[i],arr[gt-1]);
+			gt --;
+		}
+		else{//arr[i] == v
+			i++;		
+		}
+	}
+	swap(arr[l],arr[lt]);
+	
+	__quickSort3Ways(arr,l,lt-1);
+	__quickSort3Ways(arr,gt,r);
+}
 
 
 template <typename T>
