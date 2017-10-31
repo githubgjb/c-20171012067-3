7503f484b5e5a682728d8174753172bf3873a4a2
githubgjb <86742187@qq.com>
2017-10-31 14:43:34 +0800

    2017103114:43Kruskal

--- 2017103101/KruskalMST.h
+++ 2017103101/KruskalMST.h
@@ -17,11 +17,30 @@ public:
 			//对于每一个顶点找一下所有的临边
 			typename Graph::adjIterator adj(graph,i);
 			for(Edge<Weight> *e = adj.begin() ; !adj.end() ; e = adj.next()){
-				pq.insert(*e);//09:25
+				if( e->v() < e->w() ){
+					pq.insert(*e);
+				}				
 			}
 		}
 		
+		UnionFind uf(graph.V()); //开顶点个数的空间
+		//构建最小生成树           //v-1条边都是了
+		while( !pq.isEmpty() && mst.size() < graph.V() - 1){
+			//取出最小边
+			Edge<Weight> e = pq.extractMin();
+			//已经连接起来不考虑
+			if(uf.isConnected( e.v(),e.w() )){
+				continue;
+			}
+			mst.push_back();//放到集合中
+			uf.unionElements( e.v() , e.w() );			
+		}
 		
+		//计算权值
+		mstWeight = mst[0].wt();
+		for(int i=1 ; i<mst.size() ;i++){
+			mstWeight += mst[i].wt();
+		}
 	}
 	
 	~KruskalMST(){
