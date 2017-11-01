bd886ce2c1c21d392e50517d6027e5d73dee095d
githubgjb <86742187@qq.com>
2017-11-01 08:38:38 +0800

    2017110108:38

--- 2017110101/BellmanFord.h
+++ 2017110101/BellmanFord.h
@@ -8,7 +8,23 @@ private:
 	Weight* distTo;             //s到i的最短路径
 	vector<Edge<Weight>*> from; //最短路径中前一个节点是谁
 	bool hasNegativeCycle;      //是否有负权环
-		
+	
+	//检查是否有负权环
+    bool detectNegativeCycle(){
+		//进行松驰操作
+		for(int i=0 ; i < G.V() ; i++){
+			typename Graph::adjIterator adj(G,i);
+			//对每一个点的临边进行遍历
+			for(Edge<Weight>* e = adj.begin(); !adj.end() ; e = adj.next() ){
+				  //没有到达过     //满足松驰条件
+				if( !from[e->w] || distTo[e->v()] + e->wt() < distTo[e->w()] ){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
 public:
  
     BellmenFord(Graph &graph,int s):G(graph){
@@ -18,7 +34,27 @@ public:
 			from.push_back(NULL);
 		}
 		
-		//Bellman-Ford 02:23
+		//Bellman-Ford 
+		distTo[s] = Weight();  //0
+		//v-1次循环
+		for(int pass = 1 ; pass < G.V() ; pass++ ){
+			//进行松驰操作
+			for(int i=0 ; i < G.V() ; i++){
+				typename Graph::adjIterator adj(G,i);
+				//对每一个点的临边进行遍历
+				for(Edge<Weight>* e = adj.begin(); !adj.end() ; e = adj.next() ){
+					  //没有到达过     //满足松驰条件
+					if( !from[e->w] || distTo[e->v()] + e->wt() < distTo[e->w()] ){
+						distTo[e->w()] = distTo[e->v()] + e->wt();
+						from[e->w()] = e;
+					}
+				}
+			}
+		}
+		
+		hasNegativeCycle = detectNegativeCycle();
+		
+		
 	}
 
     ~BellmenFord(){
@@ -29,7 +65,7 @@ public:
 		return hasNegativeCycle;
 	}
 	
-	Weight shorttestPathTo(int w){
+	Weight shortestPathTo(int w){
 		assert(w >= 0 && w < G.V() );
 		assert(!hasNegativeCycle);
 		return distTo[w];
