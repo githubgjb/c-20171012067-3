566bbf2e9c640aa58b74129e0bad0c418d13dd33
githubgjb <86742187@qq.com>
2017-11-02 16:49:33 +0800

    20171102 16:49

--- Practice/2017110201/InsertionSort.h
+++ Practice/2017110201/InsertionSort.h
@@ -13,3 +13,102 @@ void insertionSort(T arr[] ,int n){
 		}
 	}
 } 
+
+
+template<typename T>
+void InsertionSort(T arr[],int n){	
+	for(int i=1 ; i < n ; i++){
+		T e = arr[i];
+		int j;
+		for(j = i ;j>0 && arr[j-1] > e ; j-- ){
+			arr[j] = arr[j-1];
+		}
+		arr[j] = e;
+	}
+}
+
+template<typename T>
+void InsertionSort(T arr[], int n){
+	for(int i=1 ; i<n ; i++){
+		
+		T e = arr[i];
+		int j;
+		for(j=i ; j>0 && arr[j-1] > e ; j-- ){
+			arr[j] = arr[j-1];
+		}
+		arr[j] = e;		
+	}	
+}
+
+
+template<typename T>
+void InsertSort(T arr[],int n){
+	
+	for(int i=1 ;i<n ;i++){
+		T e = arr[i];		
+		int j;
+		for(int j=i;i>0 && arr[j-1] > e ;j--){
+			arr[j] = arr[j-1];
+		}
+		arr[j] = e; 
+	}	
+}
+
+template<typename T>
+void insertionSort(T arr[] , int n){
+	
+	for(int i = 1 ; i < n ; i++ ){
+		
+		T e = arr[i];
+		int j;
+		for(j=i ; j>0 && arr[j-1] > e ; j--){
+			arr[j] = arr[j-1];
+		}
+		arr[j] = e;
+	}
+}
+
+
+void SelectionSort(int arr[] , int n){
+	
+	for(int i=0 ;i < n ;i++){
+		
+		int minIndex = i;
+		for(int j= i+1 ;j<n ;j++){
+			if(arr[j] < arr[minIndex]){
+				minIndex = j;
+			}			
+		}
+		swap(arr[i],arr[minIndex]);
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
