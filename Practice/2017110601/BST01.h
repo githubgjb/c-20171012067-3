43724502e815cea276d4b3bfd3da096fb0aebd90
githubgjb <31399236+githubgjb@users.noreply.github.com>
2017-11-06 17:57:22 +0800

    20171106 17:57

---
+++ Practice/2017110601/BST01.h
@@ -0,0 +1,46 @@
+
+
+template <typename Key,typename Value>
+class BST{
+
+private:
+   struct Node{
+	   Key key;
+	   Value value;
+	   Node *left;
+	   Node *right;	 
+
+	   Node(Key key,Value value){
+		   this->key = key;
+		   this->value = value;
+		   this->left = this ->right = NULL;
+	   }
+	Node *root; //根
+	int count;  //计数   
+
+public:
+	BST(){
+		root = NULL:
+		count = 0;
+	}
+	
+	~BST(){
+		//TODO:		
+	}
+	
+	int size(){
+		return count;
+	}
+	
+	bool isEmpty(){
+		return count == 0;
+	}
+};
+	
+		
+};
+
+int main() {
+	
+	
+}
