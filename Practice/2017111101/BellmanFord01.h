7176da434f963798086c198fe394ed8e93439271
githubgjb <86742187@qq.com>
2017-11-11 18:08:47 +0800

    20171111 18:08

--- Practice/2017111101/BellmanFord01.h
+++ Practice/2017111101/BellmanFord01.h
@@ -9,6 +9,21 @@ private:
 	Weight* distTo;
 	vector<Edge<Weight>*> from;
 	bool hasNegativeCycle; //是否有负权环
+	
+	//再做一轮对节点的松弛操作,如果超过v-1条边了,
+	//说明产生负权环了
+	bool detectNegativeCycle(){
+		for(int i=0 ; i < G.V() ; i ++){
+			typename Graph::adjIterator adj(G,i);
+			for(Edge<Weight>* e = adj.begin() ; !adj.end() ; e = adj.next()){
+				if(!from[e->w() || distTo[e->v()] + e->wt() <  distTo[e->w()] ]){
+				   return true;
+				}
+			}
+		}
+		
+		return false;
+	}
 
 public:	
    
@@ -20,6 +35,29 @@ public:
 		}
 		
 		//Bellman-Ford
+		distTo[s] = Weight();
+		
+		//对每一个点进行一个v-1次的松弛操作
+		for( int pass = 1 ; pass < G.V() ; pass++ ){
+			
+			//对所有的点的临边进行松弛操作
+			for(int i = 0 ; i<G.V() ; i++){
+				//对每一个点的临边进行遍历
+				typename Graph::adjIterator adj(G,i);
+				for(Edge<Weight>* e = adj.begin() ; !adj.end() ; e = adj.next() ){
+					//之前没有到达过     绕道i这个点再到e
+					if( !from[e->w()] || distTo[e->v()] + e->wt() < distTo[e->w()] ){
+						//找到了,就更新了
+						distTo[e->w()] = distTo[e->v()] + e->wt();
+						from[e->w()] = e;
+					}
+				}
+			}
+		}
+		
+		//判断负权环
+		hasNegativeCycle = detectNegativeCycle();
+		
 	}
 	
 	~BellmanFord(){
