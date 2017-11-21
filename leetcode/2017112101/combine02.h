f9473df9a793ad61713c9a686d226a6a4b898465
githubgjb <31399236+githubgjb@users.noreply.github.com>
2017-11-21 17:39:23 +0800

    20171121 17:39

---
+++ leetcode/2017112101/combine02.h
@@ -0,0 +1,58 @@
+
+
+#include<iostream>
+
+using namespace std;
+
+
+
+class Solution{
+
+private:
+	vector<vector<int>> res;
+
+	//当前搜索的组合的元素
+	//求解C(n,k),当前已经找到的组合存储在
+	//c中,需要从start开始搜索新的元素
+	void generateCombinations(int n,int k,int start,vector<int> &c){
+		
+		//结束条件
+		if( c.size() == k ){
+			res.push_back(c);
+			return;
+		}
+		//优化剪枝
+		//递归过程
+		//还有k - c.size()个空位
+		//所以,[i....n]中至少要有k-c.size()个元素
+		//i最多为n-(k-c.size()) + 1
+		//for(int i = start ; i <= n ; i++){
+		for(int i = start ; i <= n-(k-c.size()) + 1 ; i++){	
+			c.push_back(i);
+			generateCombinations(n , k , i+1 , c);
+			c.pop_back();
+		}
+		return;
+	}
+	
+public:
+	vector<vector<int>> combine(int n,int k){
+		
+		res.clear();
+		
+		//边界条件 4个数中找6个组合这是不可能的
+		if( n <= 0 || k <= 0 || k > n){
+			return res;
+		}
+		vector<int> c;
+		generateCombinations(n,k,1,c);
+		
+		return res;
+	}
+		
+};
+
+int main(){
+	
+	return 0;
+}
