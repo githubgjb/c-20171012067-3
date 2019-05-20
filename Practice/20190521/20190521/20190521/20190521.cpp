85967dd8a098216a9f54ebc5111ba7974dbb5d8b
githubgjb <31399236+githubgjb@users.noreply.github.com>
2019-05-21 07:15:23 +0800

    201905210715

---
+++ Practice/20190521/20190521/20190521/20190521.cpp
@@ -0,0 +1,39 @@
+// 20190521.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void selectionSort(int arr[],int n){
+	for (int i = 0; i < n; i++)
+	{
+		//寻找[i,n)区间里的最小值
+		int minIndex = i;
+		for (int j = i+1; j < n; j++)
+		{
+			if (arr[j] < arr[minIndex])
+			{
+				minIndex = j;
+			}
+		}
+		swap(arr[i],arr[minIndex]);
+	}
+
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int a[10] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+	selectionSort(a,10);
+	for (int i = 0; i < 10; i++)
+	{
+		cout << a[i] << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
+
