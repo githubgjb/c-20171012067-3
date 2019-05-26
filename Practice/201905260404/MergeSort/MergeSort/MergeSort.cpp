204c21fca6772e1cd4d7212f127bb78f137baf80
githubgjb <31399236+githubgjb@users.noreply.github.com>
2019-05-26 16:10:29 +0800

    201905260410

---
+++ Practice/201905260404/MergeSort/MergeSort/MergeSort.cpp
@@ -0,0 +1,23 @@
+// MergeSort.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+//递归使用归并排序,对arr[l...r]的范围进行排序
+template<typename T>
+void __mergeSort(T arr[],int l,int r){
+
+}
+
+template<typename T>
+void mergeSort(int arr[],int n){
+	__mergeSort(arr,0,n-1);
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
