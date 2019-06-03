c7c25184d4877a2da75da47f63ce5d3d5e9b8aa8
githubgjb <31399236+githubgjb@users.noreply.github.com>
2019-06-03 10:54:17 +0800

    201906031054

---
+++ Practice/201906030820/SortTestHelper/SortTestHelper/sortTestHelper.h
@@ -0,0 +1,39 @@
+
+#include <iostream>
+#include <ctime>
+#include <cassert>
+
+using namespace std;
+
+namespace sortTestHelper{
+
+	//生成有n个元素的的随机数组,每个元素的随机范围为[randL,rangR]
+	int* generateRandomArray(int n,int rangeL,int rangeR){
+		
+		assert(rangeL<rangeR);
+
+		int *arr = new int[n];
+		srand(time(NULL));
+
+		for (int i = 0; i < n; i++)
+		{
+			arr[i] = rand() % (rangeR - rangeL + 1) + rangeL;
+		}
+
+		return arr;
+	}
+
+	template<typename T>
+	void printArray(T arr[],int n){
+
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << " ";
+
+		}
+		cout << endl;
+	
+		return;
+	}
+
+}
