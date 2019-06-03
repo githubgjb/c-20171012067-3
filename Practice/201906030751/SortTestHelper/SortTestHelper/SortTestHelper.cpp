a84227c46d6b9c3795b5a4697e42726ff20503a9
githubgjb <31399236+githubgjb@users.noreply.github.com>
2019-06-03 08:13:21 +0800

    201906030813

---
+++ Practice/201906030751/SortTestHelper/SortTestHelper/SortTestHelper.cpp
@@ -0,0 +1,21 @@
+// SortTestHelper.cpp : 定义控制台应用程序的入口点。
+//
+
+
+#include "stdafx.h"
+#include "sortTestHelper.h"
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int n = 100000;
+	int *arr = sortTestHelper::generateRandomArray(n,0,n);
+
+	sortTestHelper::printArray(arr,n);
+
+	delete(arr);
+
+	return 0;
+}
+
