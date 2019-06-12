9fe7865b02f27bdf59c126d0ab13f72a29ed3c2f
githubgjb <31399236+githubgjb@users.noreply.github.com>
2019-06-13 06:26:14 +0800

    201906130626

--- Practice/201906130609/MaxHeap/MaxHeap/MaxHeap.cpp
+++ Practice/201906130609/MaxHeap/MaxHeap/MaxHeap.cpp
@@ -9,31 +9,11 @@
 #include <ctime>
 #include <cmath>
 #include <cassert>
+#include "MaxHeap.h"
 
 using namespace std;
 
-template <typename Item>
-class MaxHeap{
-private:
-	Item* data;
-	int count;
 
-public:
-	MaxHeap(int capacity){
-		data = new Item[capacity + 1];
-		count = 0;
-	}
-
-	~MaxHeap(){
-		delete[] data;
-	}
-	int size(){
-		return count;
-	}
-	bool isEmpty(){
-		return count == 0;
-	}
-};
 
 
 int _tmain(int argc, _TCHAR* argv[])
