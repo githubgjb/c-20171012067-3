a8a067dbe03de0b9faa145c09d64e229137da14c
githubgjb <31399236+githubgjb@users.noreply.github.com>
2019-06-24 16:11:53 +0800

    201906240411

--- Practice/201906221705/MinimumSpanTree/MinimumSpanTree/MinimumSpanTree.cpp
+++ Practice/201906221705/MinimumSpanTree/MinimumSpanTree/MinimumSpanTree.cpp
@@ -8,12 +8,69 @@
 #include "ReadGraph.h"
 #include "LazyPrimMST.h"
 #include "PrimMST.h"
+#include "KruskalMST.h"
 #include <ctime>
 
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	string filename = "testG1.txt";
+	int V = 8;
+
+	SparseGraph<double> g = SparseGraph<double>(V, false);
+	ReadGraph<SparseGraph<double>, double> readGraph(g, filename);
+	// Test Kruskal MST
+	cout << "Test Kruskal MST:" << endl;
+	KruskalMST<SparseGraph<double>, double> kruskalMST(g);
+	vector<Edge<double>> mst = kruskalMST.mstEdges();
+	for (int i = 0; i < mst.size(); i++)
+		cout << mst[i] << endl;
+	cout << "The MST weight is: " << kruskalMST.result() << endl;
+
+
+	/*
+	string filename = "testG1.txt";
+	int V = 8;
+	SparseGraph<double> g = SparseGraph<double>(V, false);
+	ReadGraph<SparseGraph<double>, double> readGraph2(g, filename);
+
+	//Test Lazy Prim MST
+	cout << "Test Lazy Prim MST" << endl;
+	LazyPrimMST<SparseGraph<double>, double> lazyPrimMST(g);
+	vector<Edge<double>> mst = lazyPrimMST.mstEdges();
+	for (int i = 0; i < mst.size(); i++)
+	{
+		cout << mst[i] << endl;
+	}
+	cout << " The MST weight is: " << lazyPrimMST.result() << endl;
+
+	cout << endl;
+
+
+	// Test Kruskal MST
+	cout << "Test Kruskal MST:" << endl;
+	KruskalMST<SparseGraph<double>, double> kruskalMST(g);
+	mst = kruskalMST.mstEdges();
+	for (int i = 0; i < mst.size(); i++)
+		cout << mst[i] << endl;
+	cout << "The MST weight is: " << kruskalMST.result() << endl;
+	*/
+	/*
+	//Test Kruskal MST
+	cout << "Test Kruskal MST" << endl;
+	KruskalMST<SparseGraph<double>, double> kruskalMST(g);
+	mst = kruskalMST.mstEdges();
+	for (int i = 0; i < mst.size(); i++)
+	{
+		cout << mst[i] << endl;
+	}
+	cout << " The MST weight is: " << kruskalMST.result() << endl;
+
+	cout << endl;
+	*/
+	/*------------------------------------------------*/
+	/*
 	//performance
 	string filename1 = "testG1.txt";
 	int V1 = 8;
@@ -89,6 +146,10 @@ int _tmain(int argc, _TCHAR* argv[])
 	PrimMST<SparseGraph<double>, double> PrimMST4(g4);
 	endTime = clock();
 	cout << "Test for G4: " << (double)(endTime - startTime) / CLOCKS_PER_SEC << " s." << endl;
+	*/
+
+
+	/*---------------------------------------------------------------------------------------------*/
 
 	/*
 	string filename = "testG1.txt";
