dd2c297cb09d66798d3cbb344c44cac9a2b755f2
githubgjb <31399236+githubgjb@users.noreply.github.com>
2019-06-26 14:15:20 +0800

    201906260215

---
+++ Practice/201906261122/BellmanFord/BellmanFord/BellmanFord.cpp
@@ -0,0 +1,43 @@
+// BellmanFord.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "SparseGraph.h"
+#include "DenseGraph.h"
+#include "ReadGraph.h"
+#include "BellmanFord.h"
+#include "BellmanFord02.h"
+
+using namespace std;
+
+// 测试Bellman-Ford算法
+int _tmain(int argc, _TCHAR* argv[])
+{
+	string filename = "testG2.txt";
+	//string filename = "testG_negative_circle.txt";
+	int V = 5;
+
+	SparseGraph<int> g = SparseGraph<int>(V, true);
+	ReadGraph<SparseGraph<int>, int> readGraph(g, filename);
+
+	cout << "Test Bellman-Ford:" << endl << endl;
+	BellmanFord2<SparseGraph<int>, int> bellmanFord(g, 0);
+	//BellmanFord<SparseGraph<int>, int> bellmanFord(g, 0);
+	if (bellmanFord.negativeCycle())
+		cout << "The graph contain negative cycle!" << endl;
+	else
+	for (int i = 1; i < V; i++) {
+		if (bellmanFord.hasPathTo(i)) {
+			cout << "Shortest Path to " << i << " : " << bellmanFord.shortestPathTo(i) << endl;
+			bellmanFord.showPath(i);
+		}
+		else
+			cout << "No Path to " << i << endl;
+
+		cout << "----------" << endl;
+	}
+
+	return 0;
+}
+
