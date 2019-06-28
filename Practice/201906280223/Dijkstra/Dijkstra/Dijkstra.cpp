7b3ee19fda512741c6f44b64caa5aece9734698f
githubgjb <31399236+githubgjb@users.noreply.github.com>
2019-06-28 14:38:32 +0800

    201906280238

--- Practice/201906280223/Dijkstra/Dijkstra/Dijkstra.cpp
+++ Practice/201906280223/Dijkstra/Dijkstra/Dijkstra.cpp
@@ -1,21 +1,19 @@
-// Krusk.cpp : 定义控制台应用程序的入口点。
+// Dijkstra.cpp : 定义控制台应用程序的入口点。
 //
 
 #include "stdafx.h"
 #include <iostream>
-#include <iomanip>
-#include "DenseGraph.h"
 #include "SparseGraph.h"
+#include "DenseGraph.h"
 #include "ReadGraph.h"
-#include "KruskalMST.h"
 #include "Dijkstra.h"
 
+using namespace std;
 
+// 测试我们的Dijkstra最短路径算法
 int _tmain(int argc, _TCHAR* argv[])
 {
-	/*
-	// 测试我们的Dijkstra最短路径算法
-	string filename = "testG2.txt";
+	string filename = "testG1.txt";
 	int V = 5;
 
 	SparseGraph<int> g = SparseGraph<int>(V, true);
@@ -35,21 +33,6 @@ int _tmain(int argc, _TCHAR* argv[])
 
 		cout << "----------" << endl;
 	}
-	*/
-	
-	string filename = "testG1.txt";
-	int V = 8;
-
-	SparseGraph<double> g = SparseGraph<double>(V, false);
-	ReadGraph<SparseGraph<double>, double> readGraph(g, filename);
-
-	// Test Kruskal MST
-	cout << "Test Kruskal MST:" << endl;
-	KruskalMST<SparseGraph<double>, double> kruskalMST(g);
-	vector<Edge<double>> mst = kruskalMST.mstEdges();
-	for (int i = 0; i < mst.size(); i++)
-		cout << mst[i] << endl;
-	cout << "The MST weight is: " << kruskalMST.result() << endl;
 
 	return 0;
 }
