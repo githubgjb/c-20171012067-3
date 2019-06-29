47a4d898ba1e44684cf50762d349117fe61427ab
githubgjb <31399236+githubgjb@users.noreply.github.com>
2019-06-29 19:43:24 +0800

    201906291943

--- Practice/201906280223/Dijkstra/Dijkstra/Dijkstra.cpp
+++ Practice/201906280223/Dijkstra/Dijkstra/Dijkstra.cpp
@@ -9,8 +9,8 @@
 #include "Dijkstra.h"
 #include "Dijkstra02.h"
 #include "Dijkstra03.h"
-
-
+#include "Dijkstra04.h"
+#include "Dijkstra05.h"
 using namespace std;
 
 // 测试我们的Dijkstra最短路径算法
@@ -25,7 +25,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	ReadGraph<SparseGraph<int>, int> readGraph(g, filename);
 
 	cout << "Test Dijkstra:" << endl << endl;
-	Dijkstra03<SparseGraph<int>, int> dij(g, 0);
+	Dijkstra05<SparseGraph<int>, int> dij(g, 0);
 	for (int i = 0; i < V; i++){
 		if (dij.hasPathTo(i)){
 			cout << "Shortest Path to " << i << " : " << dij.shortestPathTo(i) << endl;
