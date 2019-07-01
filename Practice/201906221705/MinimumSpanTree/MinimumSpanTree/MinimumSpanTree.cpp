567398e2188a4e1abf43d9ebd84fa4188babb361
githubgjb <31399236+githubgjb@users.noreply.github.com>
2019-07-01 09:01:39 +0800

    201907010901

--- Practice/201906221705/MinimumSpanTree/MinimumSpanTree/MinimumSpanTree.cpp
+++ Practice/201906221705/MinimumSpanTree/MinimumSpanTree/MinimumSpanTree.cpp
@@ -11,12 +11,29 @@
 #include "PrimMST.h"
 #include "KruskalMST.h"
 #include "KruskalMST02.h"
+#include "LazyPrimMST02.h"
 
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	string filename = "testG1.txt";
+	int V = 8;
 
+	SparseGraph<double> g = SparseGraph<double>(V, false);
+	ReadGraph<SparseGraph<double>, double> readGraph(g, filename);
+
+
+	// Test Lazy Prim MST
+	cout << "Test Lazy Prim MST:" << endl;
+	LazyPrimMST02<SparseGraph<double>, double> lazyPrimMST(g);
+	vector<Edge<double>> mst = lazyPrimMST.mstEdges();
+	for (int i = 0; i < mst.size(); i++)
+		cout << mst[i] << endl;
+	cout << "The MST weight is: " << lazyPrimMST.result() << endl;
+
+	cout << endl;
+	/*
 	//kruskalMST02  类没有问题
 	//MinHeap02     类没有问题
 	//UF02          类没有问题
@@ -33,7 +50,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	for (int i = 0; i < mst.size(); i++)
 		cout << mst[i] << endl;
 	cout << "The MST weight is: " << kruskalMST02.result() << endl;
-
+	*/
 	/*
 	string filename = "testG1.txt";
 	int V = 8;
