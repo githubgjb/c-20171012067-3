1b0b475c9c9ebc9ea2b2b9e9917ada56a73cefac
githubgjb <31399236+githubgjb@users.noreply.github.com>
2019-07-03 13:50:20 +0800

    201907030150

--- Practice/201906221705/MinimumSpanTree/MinimumSpanTree/MinimumSpanTree.cpp
+++ Practice/201906221705/MinimumSpanTree/MinimumSpanTree/MinimumSpanTree.cpp
@@ -21,6 +21,7 @@
 #include "LazyPrimMST09.h"
 #include "LazyPrimMST10.h"
 #include "PrimMST02.h"
+#include "PrimMST03.h"
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
@@ -36,7 +37,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	cout << "Test Lazy Prim MST:" << endl;
 	//LazyPrimMST10<SparseGraph<double>, double> lazyPrimMST(g);
 	//PrimMST<SparseGraph<double>, double> lazyPrimMST(g);
-	PrimMST02<SparseGraph<double>, double> lazyPrimMST(g);
+	PrimMST03<SparseGraph<double>, double> lazyPrimMST(g);
 	//PrimMST<SparseGraph<double>, double> PrimMST1(g);
 	vector<Edge<double>> mst = lazyPrimMST.mstEdges();
 	for (int i = 0; i < mst.size(); i++)
