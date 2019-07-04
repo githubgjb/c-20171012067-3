517f4b3ac6a079b293c7f9bd1465264f945ad32e
githubgjb <31399236+githubgjb@users.noreply.github.com>
2019-07-04 21:45:04 +0800

    201907042145

--- Practice/201906241806/Krusk/Krusk/Krusk.cpp
+++ Practice/201906241806/Krusk/Krusk/Krusk.cpp
@@ -12,6 +12,7 @@
 #include "KruskalMST02.h"
 #include "KruskalMST03.h"
 #include "KruskalMST04.h"
+#include "KruskalMST05.h"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -48,7 +49,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	// Test Kruskal MST
 	cout << "Test Kruskal MST:" << endl;
 	//KruskalMST<SparseGraph<double>, double> kruskalMST(g);
-	KruskalMST04<SparseGraph<double>, double> kruskalMST(g);
+	KruskalMST05<SparseGraph<double>, double> kruskalMST(g);
 	vector<Edge<double>> mst = kruskalMST.mstEdges();
 	for (int i = 0; i < mst.size(); i++)
 		cout << mst[i] << endl;
