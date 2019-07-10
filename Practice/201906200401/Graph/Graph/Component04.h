6de67ec2a6244757c6d57c5ef8c563c1a393b748
githubgjb <31399236+githubgjb@users.noreply.github.com>
2019-07-10 11:53:49 +0800

    201907101153

---
+++ Practice/201906200401/Graph/Graph/Component04.h
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+template <typename Graph>
+class Component04{
+private:
+	Graph &G;
+	bool *visited;
+	int ccount;
+	int *id;
+
+	void dfs(int v){
+		visited[v] = true;
+		id[v] = ccount;
+		typename Graph::adjIterator adj(G,v);
+		for (int i = adj.begin(); !adj.end(); i = adj.next())
+		{
+			if (!visited[i])
+			{
+				dfs(i);
+			}
+
+		}
+	}
+
+public:
+	Component04(Graph &graph):G(graph){
+		visited = new bool[G.V()];
+		id = new int[G.V()];
+		ccount = 0;
+		for (int i = 0; i < G.V(); i++)
+		{
+			visited[i] = false;
+			id[i] = -1;
+		}
+
+		for (int i = 0; i < G.V(); i++)
+		{
+			if (!visited[i])
+			{
+				dfs(i);
+				ccount++;
+			}
+
+		}
+		
+	}
+
+	~Component04(){}
+
+	int count(){
+		return ccount;
+	}
+	bool isConnected(int v,int w){
+		assert(v >= 0 && v < G.V());
+		assert(w >= 0 && w < G.V());
+		return id[v] == id[w];
+	}
+};
--- Practice/201906200401/Graph/Graph/Graph.cpp
+++ Practice/201906200401/Graph/Graph/Graph.cpp
@@ -35,6 +35,7 @@
 #include "ReadGraph08.h"
 #include "Component02.h"
 #include "Component03.h"
+#include "Component04.h"
 int _tmain(int argc, _TCHAR* argv[])
 {
 
@@ -42,7 +43,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	string filename1 = "testG1.txt";
 	SparseGraph g1 = SparseGraph(13, false);
 	ReadGraph<SparseGraph> readGraph1(g1, filename1);
-	Component03<SparseGraph> component1(g1);
+	Component04<SparseGraph> component1(g1);
 	cout << "TestG1.txt , Component Count:" << component1.count() << endl;
 
 	cout << endl;
@@ -51,7 +52,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	string filename2 = "testG2.txt";
 	SparseGraph g2 = SparseGraph(7, false);
 	ReadGraph<SparseGraph> readGraph2(g2, filename2);
-	Component03<SparseGraph> component2(g2);
+	Component04<SparseGraph> component2(g2);
 	cout << "TestG2.txt , Component Count:" << component2.count() << endl;
 
 	cout << endl;
