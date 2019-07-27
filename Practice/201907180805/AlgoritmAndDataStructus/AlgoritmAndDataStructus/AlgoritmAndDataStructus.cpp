5adbb65e41ce43ac1b9242f28585e057469df3eb
githubgjb <31399236+githubgjb@users.noreply.github.com>
2019-07-27 08:57:33 +0800

    201907270857

--- Practice/201907180805/AlgoritmAndDataStructus/AlgoritmAndDataStructus/AlgoritmAndDataStructus.cpp
+++ Practice/201907180805/AlgoritmAndDataStructus/AlgoritmAndDataStructus/AlgoritmAndDataStructus.cpp
@@ -37,6 +37,7 @@
 #include "SparseGraph.h"
 #include "DenseGraph.h"
 #include "ReadGraph.h"
+#include "Component.h"
 
 using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
@@ -44,14 +45,18 @@ int _tmain(int argc, _TCHAR* argv[])
 	string filename = "testG1.txt";
 	SparseGraph g1(13, false);
 	ReadGraph<SparseGraph> readGraph1(g1,filename);
-	g1.show();
+	//g1.show();
+	Component<SparseGraph> component1(g1);
+	cout << "testG1.txt,Component Count" << component1.count() << endl;
 
 	cout << endl;
 
-	DenseGraph g2(13, false);
-	ReadGraph<DenseGraph> readGraph2(g2,filename);
-
-	g2.show();
+	string filename2 = "testG2.txt";
+	DenseGraph g2(7, false);
+	ReadGraph<DenseGraph> readGraph2(g2, filename2);
+	//g2.show();
+	Component<DenseGraph> component2(g2);
+	cout << "testG2.txt,Component Count" << component2.count() << endl;
 
 	cout << "»Ø³µ½áÊø" << endl;
 	getchar();
